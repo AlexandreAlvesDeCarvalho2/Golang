248da8f1aec60c3931f3b085e9f8a4fc3dae67c4
Alexandre Carvalho <alexandrealvesdecarvalho2@gmail.com>
2022-05-12 23:27:34 -0300

    Func

---
+++ 4-func.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
+
+/*
+Funções
+A função pode ter zero ou mais argumentos.
+
+Neste exemplo, adicione (add) dois parâmetros do tipo int.
+
+Observe que o tipo vem após o nome da variável.
+*/
