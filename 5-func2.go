e18518c6f45874a83b6d73baa44077447a53ac3b
Alexandre Carvalho <alexandrealvesdecarvalho2@gmail.com>
2022-05-13 00:21:43 -0300

    Func2

---
+++ 5-func2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func add(x, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
+
+/*
+Funções continuação
+Quando dois ou mais parâmetros nomeados consecutivos de uma função compartilham o mesmo tipo,
+você pode omitir o tipo de todos menos o último.
+Neste exemplo, vamos encurtar
+*/
